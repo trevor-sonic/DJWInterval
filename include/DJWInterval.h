cdb86da9fdb6e0b17f05b98c4e1fdb0ec1ba0f65
Trevor <trevor.d@beydag.com>
2020-07-22 10:01:17 +0100

    Init DJWInterval

---
+++ include/DJWInterval.h
@@ -0,0 +1,38 @@
+#ifndef DJWINTERVAL_H
+#define DJWINTERVAL_H
+
+#include <Arduino.h>
+
+// ----- Callback function types -----
+extern "C" {
+typedef void (*callbackFunction)(void);
+}
+
+class DJWInterval {
+    private:
+	bool    _running = false;
+	bool    _currStatus = false;
+	bool    _enable = true;
+
+	unsigned int	_currTime = millis();
+	unsigned int	_times = 0;
+	unsigned long 	_lastMillis = 0;
+	unsigned long  	_delay = 500;
+
+    callbackFunction _functionHandler = NULL;
+
+
+
+    public:
+    DJWInterval();
+	virtual ~DJWInterval();
+
+	void setFunction        ( callbackFunction theFunction);
+	void setDelay			(unsigned long delay, unsigned int times);
+	void stop();
+	void start();
+	void restart();
+	void loop();
+};
+
+#endif
---
+++ src/DJWInterval.cpp
@@ -0,0 +1,53 @@
+#include "DJWInterval.h"
+
+DJWInterval::DJWInterval()
+{
+	// _delay		=	500;
+	// _lastMillis	=	millis();
+	// _running	=	false;
+	// _enable		=	true;
+
+	// (void*)_functionHandler;
+
+
+}
+DJWInterval::~DJWInterval()
+{
+	// TODO Auto-generated destructor stub
+}
+void DJWInterval::start()
+{
+	_lastMillis	=	millis();
+	_running	=	true;
+	_currStatus	=	false;
+	_currTime	=	_times;
+}
+void DJWInterval::stop()
+{
+	_running	=	false;
+}
+void DJWInterval::restart()
+{
+	stop();
+	start();
+}
+void DJWInterval::loop()
+{
+    if ( _running && _enable && _functionHandler && (_currTime>0 || _times==0) && (millis() >= _lastMillis + _delay) )
+    {
+    	_lastMillis	=	millis();
+		if(_currTime>0)_currTime--;
+
+		_functionHandler();
+    }
+}
+void DJWInterval::setDelay(unsigned long delay, unsigned int times)
+{
+	_delay	= delay;
+	_times	= times;
+	_currTime	= _times;
+}
+void DJWInterval::setFunction(callbackFunction theFunction)
+{
+	_functionHandler	=   theFunction;
+}
